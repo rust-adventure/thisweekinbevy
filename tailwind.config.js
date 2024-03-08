af2318e6952b492a3fe453ab57655a4612f4e72f
Christopher Biscardi <chris@christopherbiscardi.com>
2024-03-08 15:06:43 -0800

    tailwind aspect-square conflicts with aspect-ratio plugin
    remove the plugin as we don't really need it anymore

--- tailwind.config.js
+++ tailwind.config.js
@@ -362,9 +362,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/typography"),
-  ],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
