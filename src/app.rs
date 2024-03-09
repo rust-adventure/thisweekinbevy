f6f71750e8dec8d03c2d695202e239ef482f909f
Christopher Biscardi <chris@christopherbiscardi.com>
2024-03-08 17:21:00 -0800

    favicons

--- src/app.rs
+++ src/app.rs
@@ -25,6 +25,8 @@ pub fn App() -> impl IntoView {
 
     let pkg_path: &'static str =
         std::option_env!("CDN_PKG_PATH").unwrap_or("/pkg");
+    let cdn_path: &'static str =
+        std::option_env!("CDN_PATH").unwrap_or("/");
     view! {
         <Html
             lang="en"
@@ -53,6 +55,12 @@ pub fn App() -> impl IntoView {
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href=format!("{pkg_path}/this-week-in-bevy.css")/>
         <Meta name="og:site_name" content="This Week in Bevy"/>
+        <Link rel="apple-touch-icon" sizes="180x180" href=format!("{cdn_path}/apple-touch-icon.png")/>
+        <Link rel="icon" type_="image/png" sizes="32x32" href=format!("{cdn_path}/favicon-32x32.png")/>
+        <Link rel="icon" type_="image/png" sizes="16x16" href=format!("{cdn_path}/favicon-16x16.png")/>
+        <Link rel="manifest" href=format!("{cdn_path}/site.webmanifest")/>
+        <Meta name="msapplication-TileColor" content="#cdd6f4"/>
+        <Meta name="theme-color" content="#cdd6f4"/>
         <Router fallback=|| {
             let mut outside_errors = Errors::default();
             outside_errors.insert_with_default_key(AppError::NotFound);
